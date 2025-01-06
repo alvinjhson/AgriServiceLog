780b36828b84df3b7d74fa78e7a3c52248f12209
alvinjhson <alvinbest26@gmail.com>
2025-01-06 21:38:33 +0100

    login backend work with frontend

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Signup from "./signUp/signup";
+import Signin from "./signIn/signIn";
 
 const App = () => {
   return (
     <div>
-      <Signup />
+      <Signin />
     </div>
   );
 };
