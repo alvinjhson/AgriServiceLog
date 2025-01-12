aa82aec1b51ec8fdeafd1a98d5c737c8c5a7c9b4
alvinjhson <alvinbest26@gmail.com>
2025-01-12 19:57:15 +0100

    resetpassword link works

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Signup from "./signUp/signup";
 import Signin from "./signIn/signIn";
+import ResetPassword from "./resetPassword/resetPassword";
+import RequestResetPassword from "./requestPassword/requestPassword";
 
 const App = () => {
   return (
-    <div>
-      <Signin />
-    </div>
+    <Routes>
+      <Route path="/" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/request-password-reset" element={<RequestResetPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+    </Routes>
   );
 };
 
