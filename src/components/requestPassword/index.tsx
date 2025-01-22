82eff5dc3974e92c140db28eb5510bc4495e63b9
alvinjhson <alvinbest26@gmail.com>
2025-01-22 20:00:56 +0100

    change api

--- src/components/requestPassword/index.tsx
+++ src/components/requestPassword/index.tsx
@@ -10,12 +10,12 @@ const RequestResetPassword = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-
+    const API_REQUEST_PASSWORD_RESET = import.meta.env.VITE_API_REQUES_PASSWORD_RESET;
     try {
       console.log("Sending request to reset password with email:", email);
 
       const response = await axios.post(
-        "https://z09zwi52qg.execute-api.eu-north-1.amazonaws.com/auth/request-password-reset",
+        `${API_REQUEST_PASSWORD_RESET}`,
         { email },
         {
           headers: {
--- vite-env.d.ts
+++ vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
     readonly VITE_API_URL: string; 
     readonly VITE_API_RESETPASSWORD: string; 
+    readonly API_REQUEST_PASSWORD_RESET: string;
   }
   
   interface ImportMeta {
